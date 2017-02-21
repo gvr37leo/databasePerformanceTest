cb102d9214e0445d3834b2a27a5aed624a3ac0bd
Paul Boon <gvr37.p.l.boon@gmail.com>
2017-02-21 16:43:52 +0100

    ogelijk belangrijke sql statements toegevoegd

--- performanceTest/CouchDB.cs
+++ performanceTest/CouchDB.cs
@@ -17,55 +17,43 @@ namespace performanceTest {
         public string Dbname { get; set; }
         public string Tablename { get; set; }
         public void ClearDb(){
-            throw new NotImplementedException();
         }
 
         public void FillDb(int amount){
-            throw new NotImplementedException();
         }
 
         public void CreateDb(){
-            throw new NotImplementedException();
         }
 
         public void ManyJoins(){
-            throw new NotImplementedException();
         }
 
         public void ManySmallQuerys(){
-            throw new NotImplementedException();
         }
 
         public void ForceZboSpecific(){
-            throw new NotImplementedException();
         }
 
         public void IndexedSearch(){
-            throw new NotImplementedException();
         }
 
         public void NoIndexSearch(){
-            throw new NotImplementedException();
         }
 
         public void EmbeddedVsJoin(){
-            throw new NotImplementedException();
         }
 
         public void Create(){
-            throw new NotImplementedException();
         }
 
         public void Read(){
-            throw new NotImplementedException();
+            
         }
 
         public void Update(){
-            throw new NotImplementedException();
         }
 
         public void Delete(){
-            throw new NotImplementedException();
         }
     }
 }
---
+++ performanceTest/querys.sql
@@ -0,0 +1,36 @@
+ --gph
+ --Result = SessionFactory.GetCurrentSession()
+ --               .QueryOver<ZorgGphTariefWereld>()
+ --               .Where(z => z.GphCode == code)
+ --               .Fetch(z => z.Tarieven).Eager
+ --               .FutureValue().Value;
+ declare @code int = 0
+
+ select top 10 *
+ from ZorgGphTarief z inner join ZorgPrestatieTariefWereld zw on zw.Id = z.GphTariefWereld
+ where zw.class = 4 and z.GphCode = @code
+ 
+
+ --ifm
+ --Result = SessionFactory.GetCurrentSession()
+ --               .QueryOver<ZorgIfmTariefWereld>()
+ --               .Where(z => z.Artikelnummer == artikelnummer)
+ --               .Fetch(z => z.Tarieven).Eager
+ --               .FutureValue().Value;
+ declare @artikelnummer int = 0
+
+ select top 10 *
+ from ZorgIfmTarief z inner join ZorgPrestatieTariefWereld zw on zw.id = z.IfmTariefWereld
+ where zw.class = 3 and z.Artikelnummer = @artikelnummer
+
+ --tog
+ --Result = SessionFactory.GetCurrentSession().QueryOver<ZorgTogTariefWereld>()
+ --                   .Where(t => t.Prestatiecodelijst == prestatiecodelijst.Code)
+ --                   .And(t => t.DbcDeclaratiecode == dbcDeclaratiecode)
+ --                   .Fetch(t => t.Tarieven).Eager
+ --                   .FutureValue().Value;
+ declare @dbcdeclaratiecode varchar = '10B815'
+
+ select top 10 *
+ from ZorgPrestatieTariefWereld zw inner join ZorgTogTarief z on zw.id = z.TogTariefWereld
+ where zw.class = 1 and zw.DbcDeclaratiecode = @dbcdeclaratiecode
